07cfdab00a14d91f1b0187e14d8ff9970402dc4c
PAULMAX <ipaulmax@icloud.com>
2020-08-14 19:07:55 +0300

    Initial Commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Paul Max on 8/14/20.
+//  Copyright © 2020 Paul Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
